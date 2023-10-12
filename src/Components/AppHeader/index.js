6f903673a7b941e41d403aaf7f5b72061c1acf8c
Tychgen <alikxelefov@gmail.com>
2023-10-13 00:16:46 +0400

    Implemented Router, Created Header/Footer comp.

---
+++ src/Components/AppHeader/index.js
@@ -0,0 +1,12 @@
+import { Typography } from "antd";
+
+
+function AppHeader() {
+
+  return (
+    <div className="AppHeader">
+      <Typography.Title>Dashboard</Typography.Title>
+      </div>
+  );
+}
+export default AppHeader;
